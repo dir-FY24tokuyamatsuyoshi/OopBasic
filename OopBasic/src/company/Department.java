d909425d9c0547cd410b7e049ab335f18f719683
tsuyoshi.tokuyama <tsuyoshi.tokuyama.p236@dirbato.co.jp>
2024-04-08 14:48:33 +0900

    Chapter４終了

---
+++ OopBasic/src/company/Department.java
@@ -0,0 +1,12 @@
+package company;
+
+public class Department {
+    private final String nameString;
+    private final String departmentId;
+    private int badget;
+    
+    //コンストラクター
+    
+    
+
+}
